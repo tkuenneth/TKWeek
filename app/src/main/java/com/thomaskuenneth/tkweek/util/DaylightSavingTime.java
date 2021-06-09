3be29f6e156fc5aaf0afa4a2efb9fa42b8ea69c7
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2021-06-09 08:59:56 +0200

    Update DaylightSavingTime.java

--- app/src/main/java/com/thomaskuenneth/tkweek/util/DaylightSavingTime.java
+++ app/src/main/java/com/thomaskuenneth/tkweek/util/DaylightSavingTime.java
@@ -1,8 +1,25 @@
 /*
  * DaylightSavingTime.java
  *
- * TKWeek (c) Thomas Künneth 2010 - 2021
- * Alle Rechte beim Autoren. All rights reserved.
+ * Copyright 2010 - 2020 Thomas Künneth
+ * Copyright 2021 MATHEMA GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package com.thomaskuenneth.tkweek.util;
 
@@ -10,12 +27,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-/**
- * Mit Hilfe dieser Klasse kann der Beginn und das Ende der Sommerzeit ermittelt
- * werden.
- *
- * @author Thomas Künneth
- */
 public final class DaylightSavingTime {
 
     private Date begin;
@@ -33,15 +44,14 @@ public final class DaylightSavingTime {
         Date end = null;
         for (int i = cal.getActualMinimum(Calendar.DAY_OF_YEAR); i <= cal
                 .getActualMaximum(Calendar.DAY_OF_YEAR); i++) {
-            // Tag innerhalb des jahres
             cal.set(Calendar.DAY_OF_YEAR, i);
-            // Datum und Startzeit
+            // date and start time
             cal.set(Calendar.HOUR_OF_DAY, cal.getMinimum(Calendar.HOUR_OF_DAY));
             cal.set(Calendar.MINUTE, cal.getMinimum(Calendar.MINUTE));
             cal.set(Calendar.SECOND, cal.getMinimum(Calendar.SECOND));
             Date date1 = cal.getTime();
             boolean b1 = timeZone.inDaylightTime(date1);
-            // Datum und Endezeit
+            // date and end time
             cal.set(Calendar.HOUR_OF_DAY, cal.getMaximum(Calendar.HOUR_OF_DAY));
             cal.set(Calendar.MINUTE, cal.getMaximum(Calendar.MINUTE));
             cal.set(Calendar.SECOND, cal.getMaximum(Calendar.SECOND));

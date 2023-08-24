be026787be3a881a8e0fe1eda0a7c209613fa20f
Thomas Künneth <kuennetht@gmail.com>
2023-08-24 16:52:48 +0200

    Library and tools upgrades

--- app/src/main/java/com/thomaskuenneth/tkweek/fragment/AboutFragment.kt
+++ app/src/main/java/com/thomaskuenneth/tkweek/fragment/AboutFragment.kt
@@ -3,7 +3,7 @@
  *
  * Copyright 2009 - 2020 Thomas Künneth
  *           2021 MATHEMA GmbH
- *           2022 Thomas Künneth
+ *           2022 - 2023 Thomas Künneth
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in

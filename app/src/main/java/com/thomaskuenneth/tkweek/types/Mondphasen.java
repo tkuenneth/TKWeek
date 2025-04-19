2d61706df8e4d6e93e8c1f65422986342a06b7c7
Thomas Künneth <kuennetht@gmail.com>
2025-04-19 15:55:29 +0200

    Moon phases updated

--- app/src/main/java/com/thomaskuenneth/tkweek/types/Mondphasen.java
+++ app/src/main/java/com/thomaskuenneth/tkweek/types/Mondphasen.java
@@ -1,8 +1,26 @@
 /*
  * Mondphasen.java
  *
- * TKWeek (c) Thomas Künneth 2014 - 2021
- * Alle Rechte beim Autoren. All rights reserved.
+ * Copyright 2014 - 2020 Thomas Künneth
+ *           2021 MATHEMA GmbH
+ *           2022 - 2025 Thomas Künneth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package com.thomaskuenneth.tkweek.types;
 
@@ -25,11 +43,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-/**
- * Mit dieser Klasse können Mondphasen abgefragt werden.
- *
- * @author Thomas Künneth
- */
 public final class Mondphasen {
 
     private static final String TAG = Mondphasen.class.getSimpleName();
@@ -84,7 +97,7 @@ public final class Mondphasen {
                     String line;
                     while ((line = br.readLine()) != null) {
                         line = line.trim();
-                        if (line.length() > 0) {
+                        if (!line.isEmpty()) {
                             int pos = line.indexOf(',');
                             String value = line.substring(0, pos);
                             String key = line.substring(pos + 1);

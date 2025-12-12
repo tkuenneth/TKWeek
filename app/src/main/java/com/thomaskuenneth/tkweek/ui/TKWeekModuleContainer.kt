bdc3ea3bbc7ce0403fb3ded03721e0f9fedc1c5b
Thomas Künneth <kuennetht@gmail.com>
2025-12-12 19:07:46 +0100

    Refactoring

--- app/src/main/java/com/thomaskuenneth/tkweek/ui/TKWeekModuleContainer.kt
+++ app/src/main/java/com/thomaskuenneth/tkweek/ui/TKWeekModuleContainer.kt
@@ -1,16 +1,19 @@
 package com.thomaskuenneth.tkweek.ui
 
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.AnimatedPane
 import androidx.compose.material3.adaptive.layout.ThreePaneScaffoldPaneScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
-import com.thomaskuenneth.tkweek.R
 import com.thomaskuenneth.tkweek.viewmodel.UiState
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
@@ -19,23 +22,40 @@ fun ThreePaneScaffoldPaneScope.TKWeekModuleContainer(
     uiState: UiState
 ) {
     AnimatedPane {
+        val context = LocalContext.current
+        val fragmentManager = (context as AppCompatActivity).supportFragmentManager
+        val (module, arguments) = uiState.modules.last()
+        val containerId = remember { View.generateViewId() }
+
+        DisposableEffect(module, arguments) {
+            val moduleName = module.clazz.name
+            // Cleanup any existing fragment with this tag (e.g. restored from saved state)
+            // to ensure we don't have duplicates or "zombies" attached to old view IDs.
+            fragmentManager.findFragmentByTag(moduleName)?.let {
+                fragmentManager.beginTransaction().remove(it).commit()
+            }
+
+            val fragment = module.clazz.getConstructor().newInstance() as Fragment
+            fragment.arguments = arguments
+            fragmentManager.beginTransaction()
+                .replace(containerId, fragment, moduleName)
+                .commit()
+
+            onDispose {
+                fragmentManager.findFragmentByTag(moduleName)?.let {
+                    // Safe to use allowingStateLoss here because if the state is lost,
+                    // the fragment will be restored and then cleaned up by the block above
+                    // when the composable re-enters.
+                    fragmentManager.beginTransaction().remove(it).commitAllowingStateLoss()
+                }
+            }
+        }
+
         AndroidView(
             modifier = Modifier.fillMaxSize(),
-            factory = { context ->
-                FragmentContainerView(context).apply {
-                    id = R.id.fragment_container_view
-                }
-            },
-            update = { view ->
-                with(uiState.modules.last()) {
-                    val fragmentManager = (view.context as AppCompatActivity).supportFragmentManager
-                    val fragment =
-                        module.clazz.getConstructor()
-                            .newInstance() as Fragment
-                    fragment.arguments = arguments
-                    fragmentManager.beginTransaction()
-                        .replace(view.id, fragment)
-                        .commit()
+            factory = {
+                FragmentContainerView(it).apply {
+                    id = containerId
                 }
             }
         )

2e9d516531a81fb5d619113fc2ce57a2e23e626e
Thomas Künneth <kuennetht@gmail.com>
2025-12-20 18:11:50 +0100

    Nice animations ❤️

--- app/src/main/java/com/thomaskuenneth/tkweek/ui/TKWeekModuleContainer.kt
+++ app/src/main/java/com/thomaskuenneth/tkweek/ui/TKWeekModuleContainer.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
+import com.thomaskuenneth.tkweek.R
 import com.thomaskuenneth.tkweek.TKWeekModule
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
@@ -30,21 +31,27 @@ fun TKWeekModuleContainer(
         // Cleanup any existing fragment with this tag (e.g. restored from saved state)
         // to ensure we don't have duplicates or "zombies" attached to old view IDs.
         fragmentManager.findFragmentByTag(moduleName)?.let {
-            fragmentManager.beginTransaction().remove(it).commit()
+            fragmentManager.beginTransaction().remove(it).commitNow()
         }
 
         val fragment = module.clazz.getConstructor().newInstance() as Fragment
         fragment.arguments = arguments
         fragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.slide_in_right,
+                R.anim.slide_out_left,
+                R.anim.slide_in_right,
+                R.anim.slide_out_left
+            )
             .replace(containerId, fragment, moduleName)
-            .commit()
+            .commitNow()
 
         onDispose {
             fragmentManager.findFragmentByTag(moduleName)?.let {
                 // Safe to use allowingStateLoss here because if the state is lost,
                 // the fragment will be restored and then cleaned up by the block above
                 // when the composable re-enters.
-                fragmentManager.beginTransaction().remove(it).commitAllowingStateLoss()
+                fragmentManager.beginTransaction().remove(it).commitNowAllowingStateLoss()
             }
         }
     }
